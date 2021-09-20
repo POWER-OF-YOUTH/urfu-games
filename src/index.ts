c15063110a5083cf2b82cbe50607b866870ac7e2
Aceki <aceki320@gmail.com>
2021-09-20 21:40:03 +0500

    Delete .env file. Update src/index.ts, package.json

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,19 @@
-import * as dotenv from 'dotenv';
-
-dotenv.config({ path: __dirname + "/.env" });
-
+import mongoose from 'mongoose';
 import app from './app';
+
+mongoose.connect(
+    // @ts-ignore
+    process.env.MONGO_URI, 
+    {
+        useUnifiedTopology: true, 
+        useNewUrlParser: true,
+        user: process.env.MONGO_USR,
+        pass: process.env.MONGO_PWD,
+        dbName: process.env.MONGO_DBN
+    }
+)
+.then(() => console.log("Successful connection to MongoDB!"))
+.catch(err => { throw err; });
 
 const port = Number(process.env.PORT || 8000);
 const hostname = "localhost";
