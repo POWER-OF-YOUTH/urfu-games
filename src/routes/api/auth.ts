21ef4a057463f813b965066e60e46284b344d9f0
Aceki <aceki320@gmail.com>
2021-10-02 21:30:41 +0500

    Refactoring auth

--- src/routes/api/auth.ts
+++ src/routes/api/auth.ts
@@ -2,11 +2,13 @@ import express from "express";
 
 import authValidator from "../../validators/api/auth";
 import authController from "../../controllers/api/auth";
+import validateToken from "../../validators/validateToken";
 
 const authRouter = express.Router();
 
 authRouter.post("/signup", authValidator.signUp, authController.signUp);
 authRouter.post("/signin", authValidator.signIn, authController.signIn);
+authRouter.post("/check", validateToken, authController.check);
 authRouter.post("/signout", authController.signOut);
 
 export default authRouter;
