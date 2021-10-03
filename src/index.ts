2892a0d4645ea8148a35942fc072fa66167ef3e5
Aceki <aceki320@gmail.com>
2021-10-03 19:46:11 +0500

    Update README.md. Use HOSTNAME enviroment variable

--- src/index.ts
+++ src/index.ts
@@ -19,7 +19,7 @@ mongoose.connect(
     .catch(err => { throw err; });
 
 const port = Number(process.env.PORT || 8000);
-const hostname = "localhost";
+const hostname = process.env.HOSTNAME || "localhost"; 
 app.listen(port, hostname, () => {
     console.log(`Server is running on ${hostname}:${port}`);
 });
