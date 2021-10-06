5a6f935a7fc63135a0c4119d2ea81e7cb7fa8719
Aceki <aceki320@gmail.com>
2021-10-06 11:07:17 +0500

    Use rest parameter

--- src/validators/validateRole.ts
+++ src/validators/validateRole.ts
@@ -3,7 +3,10 @@ import { Request, Response, NextFunction } from "express";
 import { AccessError } from "../utils/errors";
 import { Role } from "../models/user";
 
-function validateRole(roles: Array<Role>): (req: Request, res: Response, next: NextFunction) => void {
+function validateRole(...roles: Array<Role>): (req: Request, res: Response, next: NextFunction) => void {
+    if (roles.length === 0) 
+        throw new Error("Roles is undefined.");
+
     return (req: Request, res: Response, next: NextFunction) => {
         if (!req.user)
             throw new Error("User is undefined.");
