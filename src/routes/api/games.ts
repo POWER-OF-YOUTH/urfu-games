e7c422998aa70b7e7083526747f7ed99eb863b32
Aceki <aceki320@gmail.com>
2021-10-13 14:07:41 +0500

    Add token validation to games methods

--- src/routes/api/games.ts
+++ src/routes/api/games.ts
@@ -2,13 +2,18 @@ import express from "express";
 
 import gamesValidator from "../../validators/api/games";
 import gamesController from "../../controllers/api/games";
+import validateToken from "../../validators/validateToken";
 
 const gamesRouter = express.Router();
 
 gamesRouter.get("/", gamesController.getGames);
 gamesRouter.get("/:id", gamesValidator.getGame, gamesController.getGame);
-gamesRouter.post("/", gamesValidator.addGame, gamesController.addGame);
-gamesRouter.post("/:id/upload", gamesValidator.uploadGame, gamesController.uploadGame);
-gamesRouter.delete("/:id", gamesValidator.deleteGame, gamesController.deleteGame);
+gamesRouter.post("/", validateToken, gamesValidator.addGame, gamesController.addGame);
+gamesRouter.post("/:id/upload", validateToken, gamesValidator.uploadGame, gamesController.uploadGame);
+gamesRouter.delete("/:id",
+    validateToken, 
+    gamesValidator.deleteGame, 
+    gamesController.deleteGame
+);
 
 export default gamesRouter;
