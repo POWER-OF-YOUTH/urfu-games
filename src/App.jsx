d053416314871e983a5db275a610b2a91df12327
Aceki <aceki320@gmail.com>
2021-10-27 11:47:41 +0500

    Fix App.jsx style

--- src/App.jsx
+++ src/App.jsx
@@ -5,19 +5,23 @@ import SignUpPage from "./pages/SignUpPage";
 import GamesPage from "./pages/GamesPage";
 import GamePage from "./pages/GamePage";
 import PlayPage from "./pages/PlayPage";
+import { CommentsStore } from "./models/comment";
+
+const commentsStore = CommentsStore.create({
+});
 
 function App() {
-  return (  
-      <Router>
-          <Switch>
-              <Route exact path="/signin" component={SignInPage} />
-              <Route exact path="/signup" component={SignUpPage} />
-              <Route exact path="/games" component={GamesPage} />
-              <Route exact path="/games/:gameId" component={GamePage} />
-              <Route exact path="/games/:gameId/play" component={PlayPage} />
-          </Switch>            
-      </Router>         
-  );
+    return (  
+        <Router>
+            <Switch>
+                <Route exact path="/signin" component={SignInPage} />
+                <Route exact path="/signup" component={SignUpPage} />
+                <Route exact path="/games" component={GamesPage} />
+                <Route exact path="/games/:gameId" component={GamePage} />
+                <Route exact path="/games/:gameId/play" component={PlayPage} />
+            </Switch>            
+        </Router>         
+    );
 }
 
 export default App;
