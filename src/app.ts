effb4c3b444cb4a7d9ab5440d7acb2c13514b07c
Aceki <aceki320@gmail.com>
2021-11-06 19:41:06 +0500

    Delete useless packages. Update .gitignore

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,5 @@
 import express from "express";
 import morgan from "morgan";
-import cookieParser from "cookie-parser";
 import cors from "cors";
 import mongoose from "mongoose";
 import { Request, Response, NextFunction } from "express";
@@ -15,8 +14,6 @@ app.use(cors());
 app.use(morgan("dev"));
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true }));
-app.use(cookieParser());
-
 
 // static files
 app.use(express.static(process.env.PUBLIC_DIR || "/public"));
