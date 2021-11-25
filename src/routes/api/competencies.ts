96719bcd751c5171563dd165ceeb849cae2d968c
Aceki <aceki320@gmail.com>
2021-11-25 20:41:38 +0500

    Add routes for competencies. Add validators for routes

---
+++ src/routes/api/competencies.ts
@@ -0,0 +1,33 @@
+import express from "express";
+
+import * as competenciesValidator from "../../validators/api/competencies";
+import * as competenciesController from "../../controllers/api/competencies";
+import validateToken from "../../validators/validateToken";
+
+const competenciesRouter = express.Router();
+
+competenciesRouter.post("/", 
+    validateToken, 
+    competenciesValidator.addCompetence, 
+    competenciesController.addCompetence
+);
+competenciesRouter.get("/:competenceId", 
+    competenciesValidator.getCompetence, 
+    competenciesController.getCompetence
+);
+competenciesRouter.get("/", 
+    competenciesValidator.getCompetencies, 
+    competenciesController.getCompetencies
+);
+competenciesRouter.put("/:competenceId", 
+    validateToken,
+    competenciesValidator.updateCompetence,
+    competenciesController.updateCompetence
+);
+competenciesRouter.delete("/:competenceId",
+    validateToken,
+    competenciesValidator.deleteCompetence,
+    competenciesController.deleteCompetence
+);
+
+export default competenciesRouter;
