6041d2eb4bc39a57e7328052fbe5eb6141667816
Aceki <aceki320@gmail.com>
2021-11-30 16:22:59 +0500

    Add main page link and header to NotFoundPage.

--- src/pages/error/NotFoundPage.jsx
+++ src/pages/error/NotFoundPage.jsx
@@ -1,5 +1,8 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 import { Helmet } from "react-helmet";
+
+import Header from "../../components/Header";
 
 import styles from "./NotFoundPage.module.css";
 
@@ -9,9 +12,11 @@ function NotFoundPage() {
             <Helmet>
                 <title>404</title>
             </Helmet>
+            <Header />
             <main className={styles.content}>
                 <p className={styles.errorCode}>404</p>
                 <p className={styles.errorMessage}>Запрашиваемая страница не найдена</p>
+                <NavLink className={styles.goToMain} to="/">Вернуться на главную</NavLink>
             </main>
         </>
     );
