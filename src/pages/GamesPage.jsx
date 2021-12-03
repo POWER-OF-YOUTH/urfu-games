9d4a5691b0ffb1a7a1a382fb71e9aa913b0bacdc
Aceki <aceki320@gmail.com>
2021-12-03 19:44:15 +0500

    Fix page structure

--- src/pages/GamesPage.jsx
+++ src/pages/GamesPage.jsx
@@ -1,42 +1,23 @@
 import React from "react";
-// import SearchIcon from "@material-ui/icons/Search";
-// import { Search, SearchIconWrapper, StyledInputBase } from "../styles/Default";
-import { Button, Typography, Box } from "@material-ui/core";
-import { Link } from "react-router-dom";
-import { observer } from "mobx-react-lite";
-import styles from "./GamesPage.module.css";
-import Header from "../components/Header";
-import Competence from "../components/Competence";
-import GameCard from "../components/Card";
-import Popup from "../components/Dialog";
-import PageLayout from "../layouts/PageLayout";
-import MainLayout from "../layouts/MainLayout";
-import Tags from "../components/Tags";
-import test from "../components/images/test.jpg";
 import Flickity from "react-flickity-component";
-import "flickity/css/flickity.css";
+import { Button, Typography, Box } from "@material-ui/core";
+import { Link, NavLink } from "react-router-dom";
+import { observer } from "mobx-react-lite";
+
+import Header from "../components/Header";
+import GameCard from "../components/Card";
+import Tags from "../components/Tags";
 import { useStore } from "../hooks";
+
+import "flickity/css/flickity.css";
+import styles from "./GamesPage.module.css";
 
 function GamesPage() {
     const { games } = useStore();
 
-    const flickityOptions = {
-        wrapAround: true,
-        freeSroll: true,
-        pageDots: false,
-        arrowShape: "M 0,50 L 60,00 L 50,30 L 80,30 L 80,70 L 50,70 L 60,100 Z",
-        prevNextButtons: true,
-        cellAlign: "center",
-        pauseAutoPlayOnHover: false,
-        initialIndex: 2,
-    };
     const [isMobile, setIsMobile] = React.useState(false);
 
-    // const [loading, setLoading] = useState(false);
-    
-    // const [games, setGames] = React.useState(false);
-
-    const fetchAll= async()=> {
+    const fetchAll = async ()=> {
         await games.loadGames();
     };
 
@@ -45,42 +26,65 @@ function GamesPage() {
        
         fetchAll(() => {}, () => {});
     }, []);
-
     return (        
         <>
-            {games.games ? (
-                <div>
-                    <Header />
-                    <main className={styles.content}>
-                        <Tags />
-                        <div className={styles.games}>
-                            <Box className={styles.menuButton}>
-                                <Typography variant="h6" sx={{ ml: 3, mt: 2 }} className={styles.recommendButton}>
+            <Header />
+            <main className={styles.content}>
+                <Tags />
+                <div className={styles.games}>
+                    <div className={styles.gamesWrapper}>
+                        <div className={styles.gamesBlock}>
+                            <div className={styles.captionWrapper}>
+                                <Typography variant="h5">
                                     Рекомендуемые
                                 </Typography>
-                                <Link to="/games/:gameId" style={{ color: "#000000" }}>
-                                    <Typography variant="h5" sx={{ mr: 6, mt: 2 }}>
+                                <NavLink to="/games/:gameId" className={styles.showAll}>
+                                    <Typography variant="h5">
                                         {isMobile ? "Все" : "Показать все"}
                                     </Typography>
-                                </Link>
-                            </Box>
-                            <Flickity options={flickityOptions} className={styles.slider}>
-                            </Flickity>
-                            <Typography variant="h5" sx={{ ml: 3, mt: 2 }}>
-                                Все игры
-                            </Typography>
-                            <Box className={styles.allgame} sx={{ ml: 3 }}>
+                                </NavLink>
+                            </div>
+                            <GamesCardsCarousel>
+                                { games.all().map((game, i) => (
+                                    <GameCard key={i} game={game} />
+                                ))} 
+                            </GamesCardsCarousel>
+                        </div>
+                        <div className={styles.gamesBlock}>
+                            <div className={styles.captionWrapper}>
+                                <Typography variant="h5">
+                                    Все игры
+                                </Typography>
+                            </div>
+                            <Box className={styles.gamesGrid}>
                                 { games.all().map((game, i) => (
                                     <GameCard key={i} game={game} />
                                 ))} 
                             </Box>
                         </div>
-                    </main>
+                    </div>
                 </div>
-            ) : (
-                <></>
-            )}
+            </main>
         </>
+    );
+}
+
+function GamesCardsCarousel({ children }) {
+    const flickityOptions = {
+        wrapAround: false,
+        contain: true,
+        freeSroll: false,
+        pageDots: false,
+        groupCells: true,
+        arrowShape: "M 0,50 L 60,00 L 50,30 L 80,30 L 80,70 L 50,70 L 60,100 Z",
+        prevNextButtons: true,
+        cellAlign: "center",
+    };
+
+    return (
+        <Flickity options={flickityOptions}>
+            {children}
+        </Flickity>
     );
 }
 
