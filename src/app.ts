229f45324907df7a17698bf72d0ef2c6f7af6a4e
Aceki <aceki320@gmail.com>
2022-01-16 20:41:46 +0500

    Add async start function to index.ts. Add more comments to app.ts

--- src/app.ts
+++ src/app.ts
@@ -18,7 +18,7 @@ const app = express();
 
 // middlewares
 app.use(cors());
-app.use(morgan("dev"));
+app.use(morgan("dev")); // Логгер запросов
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true }));
 // ---
--- src/index.ts
+++ src/index.ts
@@ -4,19 +4,23 @@ dotenv.config();
 import mongoose from "mongoose";
 import app from "./app";
 
-mongoose.connect(
-    <string> process.env.MONGO_URI, 
-    {
-        user: process.env.MONGO_USR,
-        pass: process.env.MONGO_PWD,
-        dbName: process.env.MONGO_DBN
-    }
-)
-    .then(() => console.log("Successful connection to MongoDB!"))
-    .catch(err => { throw err; });
+(async function () {
+    await mongoose.connect(
+        <string> process.env.MONGO_URI, 
+        {
+            user: process.env.MONGO_USR,
+            pass: process.env.MONGO_PWD,
+            dbName: process.env.MONGO_DBN
+        }
+    );
 
-const port = Number(process.env.PORT || 8000);
-const hostname = process.env.HOSTNAME || "localhost"; 
-app.listen(port, hostname, () => {
-    console.log(`Server is running on ${hostname}:${port}`);
-});
+    console.log("Successful connection to database.")
+
+    const port = Number(process.env.PORT || 8000);
+    const hostname = process.env.HOSTNAME || "localhost"; 
+
+    app.listen(port, hostname, () => {
+        console.log(`Server is running on ${hostname}:${port}.`);
+    });
+}());
+
