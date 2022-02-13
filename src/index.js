4a1f78c3ed609d3a298c68beeae2f9a6466a2237
Aceki <aceki320@gmail.com>
2022-02-13 14:47:42 +0500

    Fix style. Delete useless files

--- src/index.js
+++ src/index.js
@@ -1,19 +1,11 @@
+import "./index.css";
+
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
+
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-
 import { RootStore, RootStoreContext } from "./models/root";
-
-/* MobX заметки */
-/*
-    Если мы переходим между страницами сайта с помощью компонента Link,
-    хранилище, передаваемое через контекст, сохраняет своё состояние, 
-    т.е. не пересоздаётся.
-
-    Если обновить страницу, хранилище будет создано заново.
-*/
 
 ReactDOM.render(
     <React.StrictMode>
