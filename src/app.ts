37ce7d424344ad9b000f4f420946916b30fda473
Aceki <aceki320@gmail.com>
2022-04-12 13:13:51 +0500

    Delete useless static path from app

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,6 @@
 import cors from "cors";
 import express from "express";
 import morgan from "morgan";
-import path from "path";
 import { Request, Response, NextFunction } from "express";
 
 import { 
@@ -16,24 +15,15 @@ import apiRouter from "./routes/api";
 
 const app = express();
 
-// middlewares
 app.use(cors());
-app.use(morgan("dev")); // Requests logger
+app.use(morgan("dev")); // Логгер запросов 
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true }));
-// ---
 
-// static files
-app.use("/public", express.static(
-    path.join(process.cwd(), "/public"))
-);
-// ---
-
-// routes
 app.use("/", apiRouter);
-// ---
 
-// errors handling
+// Промежуточная функция, которая обрабатывает все виды ошибок,
+// и в зависимости от их типа возвращает ответ с определенным статусом
 app.use(<TError>(err: TError, req: Request, res: Response, next: NextFunction) => {
     const sendError = <T>(status: number, error: T) => {
         sendResponse(res, { errors: [ error ] }, status);
@@ -51,6 +41,5 @@ app.use(<TError>(err: TError, req: Request, res: Response, next: NextFunction) =
         sendError(500, new UnexpectedError(req.originalUrl));
     }
 });
-// ---
 
 export default app;
