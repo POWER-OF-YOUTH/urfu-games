4c46272df07f7a2f5cbb25d284d92e3c35a0606c
Aceki <aceki320@gmail.com>
2022-04-17 15:49:45 +0500

    Add 'UsersSearch' component. Create 'searchAPI.js' file

---
+++ src/utils/api/searchAPI.js
@@ -0,0 +1,18 @@
+import * as apiConfig from "../../helpers/apiConfig";
+
+export async function searchUsers(query = "", start, count, sort, order) {
+    const url = new URL(apiConfig.APIURL + `/search/users`);
+    url.search = new URLSearchParams({
+        q: query,
+        ...(start !== undefined && start),
+        ...(count !== undefined && count),
+        ...(sort !== undefined && sort),
+        ...(order !== undefined && order),
+    });
+
+    const response = await fetch(url, {
+        ...apiConfig.getDefaultRequestInit()
+    });
+
+    return response;
+}
