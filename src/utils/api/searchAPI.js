bf37bc5c77e22ee3c943393722b693f0d70c5e05
Константин <sakancev66@gmail.com>
2022-04-19 12:40:59 +0500

    fix components

--- src/utils/api/searchAPI.js
+++ src/utils/api/searchAPI.js
@@ -16,3 +16,20 @@ export async function searchUsers(query = "", start, count, sort, order) {
 
     return response;
 }
+
+export async function searchCompetencies(query = "", start, count, sort, order) {
+    const url = new URL(apiConfig.APIURL + `/search/competencies`);
+    url.search = new URLSearchParams({
+        q: query,
+        ...(start !== undefined && start),
+        ...(count !== undefined && count),
+        ...(sort !== undefined && sort),
+        ...(order !== undefined && order),
+    });
+
+    const response = await fetch(url, {
+        ...apiConfig.getDefaultRequestInit()
+    });
+
+    return response;
+}
