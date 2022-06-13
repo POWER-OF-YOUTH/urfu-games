53002910da02767dfc110ed7eb5df94c1fa13665
Aceki <aceki320@gmail.com>
2022-06-13 17:16:13 +0500

    Add files

---
+++ globals.js
@@ -0,0 +1,4 @@
+export const HOSTNAME = process.env.HOSTNAME ?? "localhost";
+export const PORT = Number(process.env.PORT ?? 3000);
+export const UPLOADING_DIR = process.env.UPLOADING_DIR ?? "/data"
+export const FILES_URL = "https://files.urfugames.ru"
