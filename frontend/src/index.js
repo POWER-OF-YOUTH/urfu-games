0f889df1b703ea1f3136e1c89f07cbb171f50bf1
Aceki <nikita.brekhuntsov@urfu.me>
2022-11-16 14:53:40 +0500

    Use 'createRoot' in 'index.js'

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,7 @@
 import "./index.css";
 
 import React from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
 
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
@@ -9,13 +9,14 @@ import { RootStore, RootStoreContext } from "./models/root";
 
 const rootStore = RootStore.create();
 
-ReactDOM.render(
+const container = document.getElementById("root");
+const root = createRoot(container);
+root.render(
     <React.StrictMode>
         <RootStoreContext.Provider value={rootStore}>
             <App />
         </RootStoreContext.Provider>
     </React.StrictMode>,
-    document.getElementById("root")
 );
 
 reportWebVitals();
